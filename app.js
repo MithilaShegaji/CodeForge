81d668ce6f741bc591ad57ec2f960e5f23ed6301
Mithila <mithilashegaji14@gmail.com>
2025-03-02 13:06:41 +0530

    created app.js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const port = 5000;
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const PORT = 5000;
 const mongoose = require('mongoose');
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+app.get("/", async (req, res) => {
+    return res.render("home");
+});
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+
+
+/*
+    http://localhost:5000/
+
+*/
