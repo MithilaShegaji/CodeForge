7cc016b14ce4a867f292d471b47f3981c1e76b41
Mithila <mithilashegaji14@gmail.com>
2025-03-03 09:12:24 +0530

    created dashboard.ejs

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ const mongoose = require('mongoose');
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Serve static files (CSS, JS, Images)
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
     return res.render("home");
 });
+app.get("/login", async (req, res) => {
+    return res.render("login");
+});
+app.get("/signup", async (req, res) => {
+    return res.render("signup");
+});
 
-app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+app.get("/dashboard", async (req, res) => {
+    return res.render("dashboard");
+});
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}. Click on http://localhost:5000/ \n Click on http://localhost:5000/login`));
 
 
 /*
